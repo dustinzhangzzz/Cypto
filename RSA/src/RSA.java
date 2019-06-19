7459abeedafc5c0655c819ecbee965f9fbf03d12
Dustin <zhan5472@umn.edu>
2019-06-18 23:11:26 -0500

    06/18

--- RSA/src/RSA.java
+++ RSA/src/RSA.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.util.Random;
 import java.math.BigInteger;
 public class RSA {
@@ -9,20 +10,18 @@ public class RSA {
     public  BigInteger encryptKey;
     public RSA(){
         Random random1 = new Random();
-        Random random2 = new Random();
         prime1 = BigInteger.probablePrime(512,random1);
-        prime2 = BigInteger.probablePrime(512,random2);
+        prime2 = BigInteger.probablePrime(512,random1);
         phi = prime1.subtract(BigInteger.ONE);
         phi = phi.multiply(prime2.subtract(BigInteger.ONE));//calculate phi
-        N = prime1;
-        N = N.multiply(prime2);
-        BigInteger e = phi.divide(BigInteger.TWO);
-        while(e.gcd(phi)!=BigInteger.ONE){
-           e = e.add(BigInteger.ONE);
+        N = prime1.multiply(prime2);
+        encryptKey = BigInteger.probablePrime(256, random1);
+        while(encryptKey.gcd(phi).compareTo(BigInteger.ONE)>0&& encryptKey.compareTo(phi) < 0){
+           encryptKey.add(BigInteger.ONE);
         }
         decryptKey = encryptKey.modInverse(phi);
     }
-    public boolean RSA(BigInteger first, BigInteger second, BigInteger encryptKey){
+    public RSA(BigInteger first, BigInteger second, BigInteger encryptKey){
         if (first.isProbablePrime(5)&&second.isProbablePrime(5)){
             prime1 = first;
             prime2 = second;
@@ -31,20 +30,22 @@ public class RSA {
             phi = phi.multiply(second.subtract(BigInteger.ONE));//calculate phi
             N = prime1;
             N = N.multiply(prime2);
-        }
-        else {
-            System.out.println("p1 or p2 is not a Prime, please set again");
-            return false;
-        }
-        if(phi.gcd(encryptKey)==BigInteger.ONE){
-            this.encryptKey = encryptKey;
             decryptKey = encryptKey.modInverse(phi);
-            return true;
+
         }
-        else{
-            System.out.println("EncryptKeys is not Relative prime to phi, please use same p1 and p2 to try again");
-            return false;
-        }
+//        else {
+//            System.out.println("p1 or p2 is not a Prime, please set again");
+//            return false;
+//        }
+//        if(phi.gcd(encryptKey)==BigInteger.ONE){
+//            this.encryptKey = encryptKey;
+//            decryptKey = encryptKey.modInverse(phi);
+//            return true;
+//        }
+//        else{
+//            System.out.println("EncryptKeys is not Relative prime to phi, please use same p1 and p2 to try again");
+//            return false;
+//        }
 
     }
     public BigInteger encrption(BigInteger message){
@@ -54,6 +55,17 @@ public class RSA {
     }
     public BigInteger decrption(BigInteger message){
         return message.modPow(decryptKey,N);
+    }
+
+    public static void main(String [ ] args){
+        BigInteger a = new BigInteger("5");
+        BigInteger b = new BigInteger("7");
+        BigInteger c = new BigInteger("11");
+        RSA test = new RSA();
+        BigInteger message = new BigInteger("1232333223321333333333333333333333333333333333333333");
+        System.out.println(test.encrption(message));
+        System.out.println(test.decrption(test.encrption(message)));
+        System.out.println("finished");
     }
 }
 
